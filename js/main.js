12e504a2a90eb75e9bb6fffea4f0a2693bdeac1f
takahara <takaharasuzuka@gmail.com>
2024-03-15 18:23:07 +0900

    項目名にID属性追加+クリックイベント設定

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 let tbody = document.getElementById("tbody"); //表取得
 
+let tableID = document.getElementById("tableID"); //sortボタン用
+let tableName = document.getElementById("tableName"); //sortボタン用
+let tableCom = document.getElementById("tableCom"); //sortボタン用
+let tableDivi = document.getElementById("tableDivi"); //sortボタン用
+let tableTi = document.getElementById("tableTi"); //sortボタン用
+
 //jsonファイル読み込み
 async function callApi() {
   const res = await fetch("../cards.json");
   const uesrs = await res.json();
   return uesrs;
 }
+
 //jsonファイルをhtml表示
 callApi().then((uesrs) => {
   for (let i = 0; i < uesrs.length; i++) {
@@ -22,3 +29,20 @@ callApi().then((uesrs) => {
   }
 });
 
+//sort機能
+let clickCount = 0; //クリック数によって昇順/降順を切り替える
+tableID.addEventListener("click", () => {
+  clickCount++;
+  console.log("IDクリックテスト");
+  console.log(clickCount);
+  if (clickCount == 1) {
+    alert("ID1回クリック");
+  }
+  if (clickCount == 2) {
+    alert("ID2回クリック");
+  }
+  if (clickCount == 3) {
+    clickCount = 0;
+    alert("IDクリック数初期化")
+  }
+});
