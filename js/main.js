87b3492345cebb62aa8f9f70c6bffb49a977108c
takahara <takaharasuzuka@gmail.com>
2024-03-21 16:32:37 +0900

    絞り込み機能追加

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ Vue.createApp({
       test: "テスト",
       //配列 通常状態
       list: "",
+      newlist: "",
       //sort用
       sort_key: "",
       sort_asc: true,
@@ -32,39 +33,77 @@ Vue.createApp({
         : (this.sort_asc = true);
       this.sort_key = key;
     },
-    //絞り込みワード取得
+    //絞り込み newlistに結果をpush
     searchBt(selected, search) {
       this.search_tag = selected;
       this.search_text = search;
-      alert(this.search_tag + this.search_text);
+
+      let newlist = [];
+      if (this.search_tag == "id") {
+        //エラー 型不一致？
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].id.indexOf(Number(this.search_text)) > -1) {
+            newlist.push(this.list[i]);
+          }
+        }
+      }
+
+      if (this.search_tag == "name") {
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].name.indexOf(this.search_text) > -1) {
+            newlist.push(this.list[i]);
+          }
+        }
+      }
+
+      if (this.search_tag == "company") {
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].company.indexOf(this.search_text) > -1) {
+            newlist.push(this.list[i]);
+          }
+        }
+      }
+
+      if (this.search_tag == "division") {
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].division.indexOf(this.search_text) > -1) {
+            newlist.push(this.list[i]);
+          }
+        }
+      }
+
+      if (this.search_tag == "title") {
+        for (let i = 0; i < this.list.length; i++) {
+          if (this.list[i].title.indexOf(this.search_text) > -1) {
+            newlist.push(this.list[i]);
+          }
+        }
+      }
+
+      console.log(newlist);
+
     },
   },
 
-  //算出プロパティ 計算とか変換
+  //算出プロパティ
   computed: {
     //sort
     users() {
-      // if (this.sort_key != "") {
-      //   let set = 1;
-      //   this.sort_asc ? (set = 1) : (set = -1);
+      if (this.sort_key != "") {
+        let set = 1;
+        this.sort_asc ? (set = 1) : (set = -1);
 
-      //   this.list.sort((a, b) => {
-      //     if (a[this.sort_key] < b[this.sort_key]) return -1 * set;
-      //     if (a[this.sort_key] > b[this.sort_key]) return 1 * set;
-      //     return 0;
-      //   });
+        this.list.sort((a, b) => {
+          if (a[this.sort_key] < b[this.sort_key]) return -1 * set;
+          if (a[this.sort_key] > b[this.sort_key]) return 1 * set;
+          return 0;
+        });
+      }
       //   return this.list;
       // } else {
       //   return this.list;
       // }
-
-      //絞り込み
-      let tagtest = this.search_tag;
-      for (let i = 0; i < this.list.length; i++) {
-        if (this.list[i].name == this.search_text) {
-          console.log(this.list[i]);
-        }
-      }
+      return this.list;
     },
   },
 }).mount("#app");
